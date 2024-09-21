26289595ebc618c3bad181311729c65bd0b0bed6
clarkesw <clarke.job@gmail.com>
2024-09-21 15:14:21 -0400

    First Time

---
+++ src/main/java/com/clarke/service/In28MinCurrencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clarke.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28MinCurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28MinCurrencyConversionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarke/service/In28MinCurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarke.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In28MinCurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
